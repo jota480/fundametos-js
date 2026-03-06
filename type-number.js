a313fcc7bc28250d8680ca0f5d72596aee3cf5bc
jota480 <jose.binde@escola.pr.gov.br>
2026-03-06 11:33:25 -0300

    Add files via upload

---
+++ type-number.js
@@ -0,0 +1,25 @@
+//tipo number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operaçaomatematica = primeiroNumero + segundoNumero
+
+console.log(operaçaomatematica);
+
+//ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const PontoFlutuanteSemZero = .5;
+
+const novaOperaçao = primeiroNumero / numeroPontoFlutuante
+
+console.log(novaOperaçao);
+
+//naN -> Not a Number (nao e numero)
+
+const alura = "Alura";
+
+console.log(alura * primeiroNumero);
