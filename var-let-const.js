bf008c5df8be9fbd607c81a803afee96db668311
jota480 <jose.binde@escola.pr.gov.br>
2026-03-10 14:15:23 +0000

    variaveis

---
+++ var-let-const.js
@@ -0,0 +1,21 @@
+//var
+
+//var alyura = 5;
+//var comprimento = 7;
+
+//area = altura * comprimento;
+//console.log(area);
+//var area;
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+let area;
+
+//se(forma igual 'retangulo')faça
+if (forma === 'retangulo'){
+    area = altura * comprimento;
+}else{
+    area = (altura * comprimento)/ 2;
+}
+console.log(area);
