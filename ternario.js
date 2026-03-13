7c103b5e1c3788a51c7de9e761978f373acaea99
jota480 <jose.binde@escola.pr.gov.br>
2026-03-13 07:47:18 -0300

    template

---
+++ ternario.js
@@ -0,0 +1,11 @@
+const idadeMinima = 18;
+const idadeCliente = 16;
+
+//se idadeCliente for maior que idadeMinima então faça
+if (idadeMinima >= idadeMinima){
+    consele.log("cerveja");
+}else{
+    console.log("suco")
+}
+                            //condição     "true"    "false"
+consele.log(idadeCliente >= idadeMinima? "cerveja" : "suco")
