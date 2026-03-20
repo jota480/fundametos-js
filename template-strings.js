7d43f0c3cada238bb76ca435adf77606fa39d07f
jota480 <jose.binde@escola.pr.gov.br>
2026-03-20 07:47:47 -0300

    function

--- template-strings.js
+++ template-strings.js
@@ -1,3 +1,8 @@
 const nome = "ju";
 const idade = 2026-1981;
 const cidadedeNacimento = "pitanga";
+
+//const apresentacao=" meu nome é "+ nome + ",minha idade é "+ idade +"e nasci na cidade de "cidadeNascimento";
+
+const apresentacao = ''
+console.log(apresentacao);
