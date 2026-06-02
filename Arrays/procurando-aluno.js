4820012693256a46bf4d404619e584cef92665d7
jota480 <jose.binde@escola.pr.gov.br>
2026-06-02 10:31:32 -0300

    procurando nemo

--- Arrays/procurando-aluno.js
+++ Arrays/procurando-aluno.js
@@ -6,7 +6,12 @@ const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(alunos) {
     if(listaDeAlunosEMedias[0]. includes(alunos)){
-        console.log('${aluno} esta cadastrado!')
+        const [alunos, medias]  = listaDeAlunosEMedias
+
+        const indice = alunos.indexOf(aluno);
+
+        const mediaDoAluno = medias[indice]
+        console.log('${aluno} tem a media $(mediaDoAluno).');
     }else{
         console.log ('Aluno não esta cadastrado!');
 
