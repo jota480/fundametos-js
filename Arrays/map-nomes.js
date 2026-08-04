6bd922fbb9cc5cd9b61acad5470de5e225a0fd00
jota480 <jose.binde@escola.pr.gov.br>
2026-08-04 10:33:04 -0300

    objeto

---
+++ Arrays/map-nomes.js
@@ -0,0 +1,7 @@
+const nomes = ["ana julia", "caio vinicius", "bia silva"];
+
+const nomesPadronizados = nomes.map((nome) => {
+
+});
+
+console.log(nomesPadronizados);
---
+++ objetos/objetos.js
@@ -0,0 +1,5 @@
+const listaCPF = [1111111, 222222, 3333];
+
+const informaçõesPessoa = ["nome", "Josep", "idade", 32, "CPF", "11112222333" ];
+
+console.log(informaçõesPessoa(1));
