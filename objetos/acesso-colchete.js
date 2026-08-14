d81b9dcae570d6909176f7272538f8fc777d2af3
jota480 <jose.binde@escola.pr.gov.br>
2026-08-14 08:30:52 -0300

    67

---
+++ objetos/acesso-colchete.js
@@ -0,0 +1,15 @@
+const clente = {
+    nome: "José"
+    idade: 15,
+    cpf: 11122233344
+    email: joselucas@gmail.com
+};
+
+console.console.log('o nome do cliente ie ${cliente["nome"]} e essa pessoa tem {cliente["idade]} anos.');
+
+const chaves = ["nome", "idade", "cpf", "email"];
+
+chaves.console.log.forEach(element => {
+    console.log('A chave ${chave} tem valor &{cliente[chave]}');
+    
+});
